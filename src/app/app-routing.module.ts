2d9a8566d466b77f860c0744863c80637c68389b
Hassan Khousheish <hassan@crunchrapps.com>
2022-11-06 16:15:55 +0100

    Add Not Found Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+  },
+  {
+    path: '404',
+    component: NotFoundComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
