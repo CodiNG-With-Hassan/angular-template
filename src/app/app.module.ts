ef236a88d159820bffddab84beb77a45d80afe42
Hassan Khousheish <hassan@crunchrapps.com>
2022-11-08 21:08:03 +0100

    Add @NGRX Infrastructure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,12 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { getInitialState, REDUCER_PROVIDER, reducerToken } from '@AppStore';
+import { environment } from '@Environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,8 +30,16 @@ import { SharedModule } from './shared/shared.module';
         deps: [HttpClient],
       },
     }),
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot(reducerToken, { initialState: getInitialState}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
-  providers: [],
+  providers: [
+    REDUCER_PROVIDER,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
