add2b7ae84d8a2840f197d8d66ae7c200ec9cb56
Hassan Khousheish <khousheish@live.com>
2025-07-03 21:11:17 +0200

    Upgrade To V20

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,22 +7,9 @@ import { AppComponent } from './app.component';
 import { NotFoundComponent } from './shared/pages/not-found/not-found.component';
 
 const routes: Routes = [
-  {
-    path: AppRoutes.Empty,
-    component: AppComponent,
-  },
-  {
-    path: AppRoutes.Other,
-    redirectTo: AppRoutes.NotFound,
-  },
-  {
-    path: AppRoutes.NotFound,
-    component: NotFoundComponent,
-  },
+  { path: AppRoutes.Other, redirectTo: AppRoutes.NotFound },
+  { path: AppRoutes.NotFound, component: NotFoundComponent },
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
+@NgModule({ imports: [RouterModule.forRoot(routes)], exports: [RouterModule] })
 export class AppRoutingModule {}
